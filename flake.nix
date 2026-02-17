6b950ac7fedb2f3c652404e7681a61832229eb1e
fish <kiokunee@gmail.com>
2026-02-17 22:42:53 +0800

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Home Manager configuration of fish";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    dotfiles = {
+      url = "git+file:///home/fish/Documents/projects/nix-flakes/dotfiles";
+    };
+
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    substratum = {
+      url = "github:Ryokune/substratum";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      stylix,
+      dotfiles,
+      nvf,
+      substratum,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."fish" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+          nvf.homeManagerModules.default
+          stylix.homeModules.stylix
+          dotfiles.homeModules.default
+          substratum.homeModules.default
+          {
+            nixpkgs.overlays = [
+              substratum.overlays.default
+            ];
+          }
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,429 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  # # Home Manager needs a bit of information about you and the paths it should
+  # # manage.
+  # # Workaround to not being able to have zsh as the default shell via home manager. sucks.
+  # programs.bash = {
+  #   enable = true;
+  #   # This replaces the bash process with zsh for interactive logins
+  #   initExtra = ''
+  #     if [[ $- == *i* ]]; then
+  #       exec zsh
+  #     fi
+  #   '';
+  # };
+  home.enableNixpkgsReleaseCheck = false;
+  systemd.user.services.waydroid-session = {
+    Unit = {
+      Description = "Waydroid User Session";
+      After = [ "waydroid-container.service" ];
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.waydroid-nftables}/bin/waydroid session start";
+      ExecStop = "${pkgs.waydroid-nftables}/bin/waydroid session stop";
+
+      # Clean up processes on exit
+      KillMode = "mixed";
+
+      # Resource limits: 4GB Max, 3GB cache threshold
+      MemoryMax = "4G";
+      MemoryHigh = "3G";
+
+      TimeoutStopSec = "15s";
+      Restart = "on-failure";
+    };
+  };
+
+  systemd.user.services.swww = {
+    Unit = {
+      Description = "Wayland wallpaper daemon";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.swww}/bin/swww-daemon";
+      ExecStartPost = "${pkgs.swww}/bin/swww img ${config.stylix.image} --transition-type fade";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
+
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    image = ./ado.jpeg;
+    polarity = "dark";
+    opacity.terminal = 0.8;
+    icons = {
+      enable = true;
+      package = pkgs.papirus-icon-theme;
+
+      # You can specify the dark/light variants
+      dark = "Papirus-Dark";
+      light = "Papirus-Light";
+    };
+    fonts = {
+      monospace = {
+        package = pkgs.maple-mono.CN;
+        name = "Maple Mono CN";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 11;
+        terminal = 11.5;
+        desktop = 12;
+        popups = 12;
+      };
+
+    };
+
+    targets = {
+      nvf.enable = false;
+      mako = {
+        fonts.override = {
+          serif = config.stylix.fonts.monospace;
+          sansSerif = config.stylix.fonts.monospace;
+          emoji = config.stylix.fonts.monospace;
+        };
+      };
+      firefox = {
+        colors.enable = true;
+        colorTheme.enable = true;
+        profileNames = [ "default" ];
+      };
+      alacritty = {
+        colors.enable = false;
+      };
+      fuzzel = {
+        fonts.override = {
+          serif = config.stylix.fonts.monospace;
+          sansSerif = config.stylix.fonts.monospace;
+          emoji = config.stylix.fonts.monospace;
+        };
+      };
+    };
+  };
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+    pinentry.package = pkgs.pinentry-curses;
+  };
+
+  programs = {
+    gpg = {
+      enable = true;
+    };
+    git = {
+      enable = true;
+      signing = {
+        key = "10928E38A063FB75";
+        signByDefault = true;
+      };
+      settings = {
+        user = {
+          name = config.home.username;
+          email = "kiokunee@gmail.com";
+        };
+        safe.directory = "/etc/nixos";
+        init = {
+          defaultBranch = "main";
+        };
+      };
+    };
+    firefox = {
+      enable = true;
+      profiles = {
+        default = {
+          extensions.force = true;
+        };
+      };
+    };
+
+    # neovim = {
+    #   enable = true;
+    #   defaultEditor = true;
+    # };
+
+    nvf = {
+      enable = true;
+
+      # Your settings need to go into the settings attribute set
+      # most settings are documented in the appendix
+      settings = {
+        vim = {
+          comments = {
+            comment-nvim.enable = true;
+          };
+          autopairs = {
+            nvim-autopairs.enable = true;
+          };
+          viAlias = false;
+          vimAlias = true;
+          undoFile.enable = true;
+          preventJunkFiles = true;
+          tabline.nvimBufferline.enable = true;
+
+          options = {
+            clipboard = "unnamedplus";
+            ignorecase = true;
+            mouse = "a";
+            autoindent = true;
+            smartindent = true;
+            tabstop = 2;
+            shiftwidth = 2;
+            softtabstop = 2;
+            expandtab = true;
+            updatetime = 50;
+          };
+
+          statusline.lualine.enable = true;
+          telescope.enable = true;
+          autocomplete.blink-cmp.enable = true;
+          treesitter.enable = true;
+          visuals.fidget-nvim.enable = true;
+          binds.whichKey.enable = true;
+          filetree.neo-tree.enable = true;
+
+          diagnostics = {
+            enable = true;
+            config = {
+              update_in_insert = true;
+              signs.text = lib.generators.mkLuaInline ''
+                {
+                  [vim.diagnostic.severity.ERROR] = "󰅚 ",
+                  [vim.diagnostic.severity.WARN] = "󰀪 ",
+                }
+              '';
+              virtual_text = {
+                format = lib.generators.mkLuaInline ''
+                  function(diagnostic)
+                    return string.format("%s (%s)", diagnostic.message, diagnostic.source)
+                  end
+                '';
+              };
+            };
+          };
+
+          lsp = {
+
+            trouble.enable = true;
+            formatOnSave = true;
+            enable = true;
+            lspconfig.enable = true;
+            servers = {
+              nixd = {
+                settings = {
+                  nixd = {
+                    nixpkgs = {
+                      expr = "import <nixpkgs> { }";
+                    };
+                    options = {
+                      flake_parts = {
+                        expr = ''
+                          let
+                              flake = builtins.getFlake (toString ./.);
+
+                              debugOptions = 
+                                if flake ? debug && flake.debug ? options
+                                then flake.debug.options
+                                else {};
+
+                              systemOptions = 
+                                if flake ? currentSystem && flake.currentSystem ? options
+                                then flake.currentSystem.options
+                                else {};
+                            in
+                              debugOptions // systemOptions
+                        '';
+                      };
+
+                      home_manager = {
+                        expr = ''
+                          let
+                            flake = builtins.getFlake (toString ./.);
+                            options =
+                              if flake ? homeConfigurations
+                                 && flake.homeConfigurations ? ${config.home.username}
+                              then flake.homeConfigurations.${config.home.username}.options
+                              else {};
+                          in
+                            options
+                        '';
+                      };
+                      nixos = {
+                        expr = ''
+                          let
+                            flake = builtins.getFlake (toString ./.);
+                            options =
+                              if flake ? nixosConfigurations
+                                 && flake.nixosConfigurations ? siesta
+                              then flake.nixosConfigurations.siesta.options
+                              else {};
+                          in
+                            options
+                        '';
+                      };
+                    };
+                  };
+                };
+              };
+            };
+
+          };
+          languages = {
+            enableTreesitter = true;
+            nix = {
+              enable = true;
+              lsp.servers = [ "nixd" ];
+            };
+          };
+        };
+      };
+    };
+
+    zsh = {
+      enable = true;
+      initContent = ''
+        source $HOME/.zshrc.unix
+      '';
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+    };
+    starship = {
+      enable = true;
+    };
+    waybar = {
+      enable = true;
+      systemd.enable = true;
+    };
+    alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          blur = true; # Optional: Enables blurring if supported by compositor
+        };
+      };
+    };
+    fuzzel = {
+      enable = true;
+      # settings = {
+      #   main = {
+      #     font = "Maple Mono NF CN:size=12";
+      #   };
+      # };
+    };
+    bat = {
+      enable = true;
+    };
+    fastfetch = {
+      enable = true;
+    };
+    eza.enable = true;
+
+  };
+
+  home.username = "fish";
+  home.homeDirectory = "/home/fish";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  services = {
+    gnome-keyring.enable = true;
+    mako = {
+      enable = true;
+      settings = {
+        default-timeout = 8000;
+        border-radius = 8;
+        border-size = 2;
+      };
+    };
+  };
+  home.packages = with pkgs; [
+    substratum.hayase
+    substratum.wake-home
+    seahorse
+    ripgrep
+    trash-cli
+    swww
+    wl-clipboard
+    vesktop
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/fish/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    NH_HOME_FLAKE = "${config.home.homeDirectory}/.config/home-manager";
+  };
+
+  # Let Home Manager install and manage itself.
+  # programs.home-manager.enable = true;
+}
